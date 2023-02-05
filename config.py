73198bd04c0007291fad79b60e4b360830b3e54f
unknown <pravinlinge92@gmail.com>
2023-02-05 10:18:02 +0530

    Config File Modified

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 DATA = r"CSV_File\heart.csv"
 
-MODEL = r"artifacts\KNN_rscv_model.pkl"
+MODEL = r"artifacts/KNN_rscv_model.pkl"
 
-NORMAL_SCAL = r'artifact\Normal_scaler.pkl'
+NORMAL_SCAL = r'artifact/Normal_scaler.pkl'
 
 PORT  = 5555
